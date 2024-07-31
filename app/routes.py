b847d9ff53e13157097d73b185dc5e7f9a131761
Luciano Bermudez <lucianobmecheng69@gmail.com>
2024-07-31 11:40:22 -0700

    added license

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 
-routes = Blueprint("route",__name__)
+routes = Blueprint("routes", __name__)
+
 @routes.route('/', methods=['GET', 'POST'])
 @routes.route('/home', methods=['GET', 'POST'])
-
-
 def home():
     return render_template("home.html")
+
+@routes.route('/license')
+def license():
+    return render_template('license.html')
