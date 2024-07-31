de64c6263deb0d5b2c76b35db0b7b9e217137d96
Luciano Bermudez <lucianobmecheng69@gmail.com>
2024-07-31 11:13:15 -0700

    Added csv path hopefully right

--- src/llm_generators/CodeGenerators.py
+++ src/llm_generators/CodeGenerators.py
@@ -77,9 +77,7 @@ class CodeGenerator(LLMConfig):
         return generated_code
 
 # Custom Generators
-base_path = os.path.dirname(os.path.abspath(__file__))
-csv_path = os.path.join(base_path, "mechedu1.0\src\data\Question_Embedding_20240128.csv")
-
+csv_path = "data/Question_Embedding_20240128.csv"
 
 question_html_generator = CodeGenerator(
     embedding_column="question_embedding",
