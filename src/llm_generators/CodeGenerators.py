4154fb9abdb0253bc8713ab8f0e072841405837b
Luciano Bermudez <lucianobmecheng69@gmail.com>
2024-07-31 10:45:43 -0700

    Added stuff

--- src/llm_generators/CodeGenerators.py
+++ src/llm_generators/CodeGenerators.py
@@ -76,7 +76,7 @@ class CodeGenerator(LLMConfig):
         return generated_code
 
 # Custom Generators
-csv_path = r"mechedu1.0\src\data\Question_Embedding_20240128.csv"
+csv_path = r"mechedu1.0\src\llm_generators\Question_Embedding_20240128.csv"
 question_html_generator = CodeGenerator(
     embedding_column="question_embedding",
     llm_model="gpt-4o",
